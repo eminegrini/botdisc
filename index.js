c4253e4c6f1fd077b69d2565c78bc92a41552bbf
Emiliano Negrini <emi.negrini@gmail.com>
2020-07-27 20:29:44 -0300

    bot

--- index.js
+++ index.js
@@ -1,25 +1,80 @@
-require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
 
-bot.login(TOKEN);
+const token = 'NzM0ODk2MDM2Mjc5MjIyMzc0.XxYXRA.4R7tFnHpts4aVBujVcsKpvVbM-Y';
+const PREFIX ='.';
+var servers = {};
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
-});
+const ytdl = require("ytdl-core");
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+bot.on('ready',()=>{
+    console.log('This bot is online!')
+})
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+bot.on('message',message =>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+    switch(args[0]){
+        case 'play':
+            
+            function play(connection,message){
+                var server = servers[message.guild.id];
+
+                server.dispatcher = connection.playStream(ytdl(server.queue[0],{filter: 'audioonly'}));
+                server.queue.shift();
+                server.dispatcher.on("end",function(){
+                    if(server.queue[0]){
+                        play(connection,message);
+                    }else {
+                        connection.disconnect();
+                    }
+                });
+
+            }
+
+            if(!args[1]){
+                message.channel.send("you need to provide a link");
+                return;
+            }
+            if(!message.member.voiceChannel){
+                message.channel.send("you must be in a channel to play the bot !");
+                return;
+            }
+
+            if(!servers[message.guild.id]) servers[message.guild.id]={
+                queue :[]
+            }
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if(!message.guild.voiceConnection) 
+            message.member.voiceChannel.join().then(function(connection){
+                play(connection,message);
+            })
+
+            break;
+        case 'skip':
+                var server = servers[message.guild.id];
+                if(server.dispatcher) server.dispatcher.end();
+                message.channel.send("song skipped")
+        break;
+
+        case 'stop':
+                var server = servers[message.guild.id];
+                if(message.guild.voiceConnection){
+                    for(var i=server.queue.length -2;i>=0;i--){
+                        server.queue.splice(i,1);
+                    }
+                    server.dispatcher.end();
+                    message.channel.send("Ending the queue leaving the voice channel")
+                    console.log('stopped the queue')
+                }
+                if(message.guild.connection) message.guild.voiceConnection.disconnect();
+        
+        break;
     }
-  }
-});
+})
+
+
+bot.login(token)
